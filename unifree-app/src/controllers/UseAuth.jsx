c1849e6c8eca52d48c797d3bd908a96e8ed963a0
filexft <filimonteweldeft@gmail.com>
2023-12-08 00:09:31 +0100

    pull from remote

---
+++ unifree-app/src/controllers/UseAuth.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const jsC = require('js-cookie');
+
+
+const UseAuth = () => {
+    const [user, setUser] = useState();
+    const [user, setUser] = useState();
+    const [user, setUser] = useState();
+    
+    fetch({
+        method : "POST",
+    })
+
+}
+
+
+
+export default UseAuth;
